8c7ceeaf2302d8679f31924e66b904d8d3617406
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-20 13:17:27 +0700

    configuring ApiResponse

--- core/src/main/java/com/asthiseta/core/data/soure/remote/network/ApiResponse.kt
+++ core/src/main/java/com/asthiseta/core/data/soure/remote/network/ApiResponse.kt
@@ -1,4 +1,6 @@
 package com.asthiseta.core.data.soure.remote.network
 
-class ApiResponse {
+sealed class ApiResponse<out T> {
+    data class Success<out T>(val data: T) : ApiResponse<T>()
+    data class Error(val errorMessage : String?) : ApiResponse<Nothing>()
 }
