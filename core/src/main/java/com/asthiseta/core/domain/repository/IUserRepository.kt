75cebe074ccd330d6c0c4f0b870ed63d39e0df6a
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-20 16:06:36 +0700

    added code interface for user repository, and user repository

--- core/src/main/java/com/asthiseta/core/domain/repository/IUserRepository.kt
+++ core/src/main/java/com/asthiseta/core/domain/repository/IUserRepository.kt
@@ -1,4 +1,17 @@
 package com.asthiseta.core.domain.repository
 
+import com.asthiseta.core.data.Resource
+import com.asthiseta.core.domain.model.User
+import kotlinx.coroutines.flow.Flow
+
 interface IUserRepository {
+    fun getAllUsers(): Flow<Resource<List<User>>>
+
+    fun getAllFollowers(username: String): Flow<Resource<List<User>>>
+
+    fun getAllFollowing(username: String): Flow<Resource<User>>
+
+    //fun getDetailUser(username: String): Flow<Resource<User>>
+
+    //fun getDetailState(username: String): Flow<User>?
 }
