16ed80c48583e33b49fb08c4fabf7e2e7b547414
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-20 17:56:46 +0700

    fookin build config

--- core/src/main/java/com/asthiseta/core/di/CoreModule.kt
+++ core/src/main/java/com/asthiseta/core/di/CoreModule.kt
@@ -1,4 +1,13 @@
 package com.asthiseta.core.di
 
-class CoreModule {
+import org.koin.dsl.module
+import com.asthiseta.core.BuildConfig
+
+private const val BASE_URL = BuildConfig.BASE_URL
+private const val API_KEY = BuildConfig.API_KEY
+
+val networkModule = module {
+    single {
+
+    }
 }
