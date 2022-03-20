f9974406a36a283cba7f3b1e1f8a9dae848371e3
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-20 18:24:57 +0700

    added OkHttp request configuration

--- core/src/main/java/com/asthiseta/core/di/CoreModule.kt
+++ core/src/main/java/com/asthiseta/core/di/CoreModule.kt
@@ -2,12 +2,34 @@ package com.asthiseta.core.di
 
 import org.koin.dsl.module
 import com.asthiseta.core.BuildConfig
+import okhttp3.CertificatePinner
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
 
 private const val BASE_URL = BuildConfig.BASE_URL
 private const val API_KEY = BuildConfig.API_KEY
 
 val networkModule = module {
     single {
+        val hostname = "api.github.com"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/ORtIOYkm5k6Nf2tgAK/uwftKfNhJB3QS0Hs608SiRmE=")
+            .add(hostname, "sha256/k2v657xBsOVe1PQRwOsHsw3bsGT2VzIqz5K+59sNQws=")
+            .add(hostname, "sha256/uyPYgclc5Jt69vKu92vci6etcBDY8UNTyrHQZJpVoZY=")
+            .build()
 
+        OkHttpClient.Builder()
+            .addInterceptor{chain ->
+                val original = chain.request()
+                val requestBuilder = original.newBuilder().header("Authorization", API_KEY)
+                val request = requestBuilder.build()
+                chain.proceed(request)
+            }
+            .connectTimeout(1, TimeUnit.MINUTES)
+            .readTimeout(1, TimeUnit.MINUTES)
+            .certificatePinner(certificatePinner)
+            .build()
     }
+
+
 }
