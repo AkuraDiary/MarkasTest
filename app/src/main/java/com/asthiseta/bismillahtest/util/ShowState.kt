ceff9f911c82514d2e8f8cb2d23ad0ee2ba55c6f
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-20 23:32:56 +0700

    added function in show states

--- app/src/main/java/com/asthiseta/bismillahtest/util/ShowState.kt
+++ app/src/main/java/com/asthiseta/bismillahtest/util/ShowState.kt
@@ -1,4 +1,11 @@
 package com.asthiseta.bismillahtest.util
 
+import com.asthiseta.bismillahtest.databinding.FollowFragmentBinding
+import com.asthiseta.bismillahtest.databinding.HomeFragmentBinding
+
 interface ShowState {
+
+    fun onSuccessState(homeFragmentBinding : HomeFragmentBinding? = null, followFragmentBinding: FollowFragmentBinding? = null)
+    fun onLoadingState(homeFragmentBinding : HomeFragmentBinding? = null, followFragmentBinding: FollowFragmentBinding? = null)
+    fun onErrorState(homeFragmentBinding : HomeFragmentBinding? = null, followFragmentBinding: FollowFragmentBinding? = null)
 }
