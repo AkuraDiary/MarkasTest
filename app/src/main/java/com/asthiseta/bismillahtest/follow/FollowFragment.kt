318f3a38436cbcb6ca14a2a7829aee7389a31752
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-21 19:41:18 +0700

    added some private variable, mau nugas dlu ges

--- app/src/main/java/com/asthiseta/bismillahtest/follow/FollowFragment.kt
+++ app/src/main/java/com/asthiseta/bismillahtest/follow/FollowFragment.kt
@@ -1,11 +1,24 @@
 package com.asthiseta.bismillahtest.follow
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.asthiseta.bismillahtest.databinding.FollowFragmentBinding
 import com.asthiseta.bismillahtest.databinding.HomeFragmentBinding
 import com.asthiseta.bismillahtest.util.ShowState
+import com.asthiseta.core.ui.UserAdapter
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class FollowFragment : Fragment() , ShowState{
+
+    private var _followBinding : FollowFragmentBinding? = null
+    private  val followBinding get() = _followBinding!!
+
+    private lateinit var followAdapter: UserAdapter
+    private lateinit var username: String
+
+    private var type: String? = null
+    private val followViewModel: FollowViewModel by viewModel()
+
     override fun onSuccessState(
         homeFragmentBinding: HomeFragmentBinding?,
         followFragmentBinding: FollowFragmentBinding?
@@ -26,5 +39,18 @@ class FollowFragment : Fragment() , ShowState{
         message: String?
     ) {
         TODO("Not yet implemented")
+    }
+
+    companion object{
+        fun newInstance(username: String, type: String) =
+            FollowFragment().apply {
+                arguments = Bundle().apply {
+                    putString(USERNAME, username)
+                    putString(TYPE, type)
+                }
+            }
+
+        private const val USERNAME ="username"
+        private const val TYPE = "type"
     }
 }
