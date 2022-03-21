97bc4971c1e78cf871e8278ba6f1bd52af7fe403
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-21 19:30:30 +0700

    added follow fragment

--- app/src/main/java/com/asthiseta/bismillahtest/follow/FollowFragment.kt
+++ app/src/main/java/com/asthiseta/bismillahtest/follow/FollowFragment.kt
@@ -1,6 +1,30 @@
 package com.asthiseta.bismillahtest.follow
 
 import androidx.fragment.app.Fragment
+import com.asthiseta.bismillahtest.databinding.FollowFragmentBinding
+import com.asthiseta.bismillahtest.databinding.HomeFragmentBinding
+import com.asthiseta.bismillahtest.util.ShowState
 
-class FollowFragment : Fragment() {
+class FollowFragment : Fragment() , ShowState{
+    override fun onSuccessState(
+        homeFragmentBinding: HomeFragmentBinding?,
+        followFragmentBinding: FollowFragmentBinding?
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onLoadingState(
+        homeFragmentBinding: HomeFragmentBinding?,
+        followFragmentBinding: FollowFragmentBinding?
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onErrorState(
+        homeFragmentBinding: HomeFragmentBinding?,
+        followFragmentBinding: FollowFragmentBinding?,
+        message: String?
+    ) {
+        TODO("Not yet implemented")
+    }
 }
