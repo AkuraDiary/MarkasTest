61115939e5dc152c76c221339a5b6fe2c440bc90
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-21 11:35:51 +0700

    added code in HomeFragment

--- app/src/main/java/com/asthiseta/bismillahtest/home/HomeFragment.kt
+++ app/src/main/java/com/asthiseta/bismillahtest/home/HomeFragment.kt
@@ -1,6 +1,125 @@
 package com.asthiseta.bismillahtest.home
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.IdRes
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavGraph
+import androidx.navigation.fragment.FragmentNavigatorExtras
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.asthiseta.bismillahtest.R
+import com.asthiseta.bismillahtest.databinding.FollowFragmentBinding
+import com.asthiseta.bismillahtest.databinding.HomeFragmentBinding
+import com.asthiseta.bismillahtest.util.ShowState
+import com.asthiseta.core.ui.UserAdapter
+import org.koin.android.ext.android.getKoin
+import org.koin.android.viewmodel.ViewModelParameter
+import org.koin.android.viewmodel.koin.getViewModel
+import org.koin.core.parameter.ParametersDefinition
+import org.koin.core.qualifier.Qualifier
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), ShowState {
+
+    private inline fun<reified VM : ViewModel> Fragment.sharedGraphViewModel(
+        @IdRes navGraphId : Int,
+        qualifier : Qualifier? = null,
+        noinline parameters: ParametersDefinition?=null
+    ) = lazy{
+        val store = findNavController().getViewModelStoreOwner(navGraphId).viewModelStore
+        getKoin().getViewModel(ViewModelParameter(VM::class, qualifier, parameters, store))
+    }
+
+    private var _homeBinding : HomeFragmentBinding? = null
+    private val homeBinding get() = _homeBinding!!
+    private lateinit var homeAdapter : UserAdapter
+    private val homeVM : HomeViewModel by sharedGraphViewModel(R.id.user_navigation)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        val actionBar = (activity as AppCompatActivity).supportActionBar
+        actionBar?.title = getString(R.string.auth_usrname)
+        _homeBinding = HomeFragmentBinding.inflate(layoutInflater, container, false)
+        return homeBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setHasOptionsMenu(true)
+
+        //homeBinding.errLayout.emptyText.text
+
+        homeAdapter = UserAdapter(arrayListOf()) {username, iv ->
+            findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToDetailFragment(username),
+                FragmentNavigatorExtras(iv to username)
+            )
+        }
+
+        homeBinding.recyclerHome.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            adapter = homeAdapter
+        }
+
+        observeHome()
+    }
+
+    private fun observeHome() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSuccessState(
+        homeFragmentBinding: HomeFragmentBinding?,
+        followFragmentBinding: FollowFragmentBinding?
+    ) {
+        homeFragmentBinding?.apply {
+            errLayout.mainNotFound.visibility = View.GONE
+            progress.visibility = View.GONE
+            recyclerHome.visibility = View.VISIBLE
+        }
+    }
+
+    override fun onLoadingState(
+        homeFragmentBinding: HomeFragmentBinding?,
+        followFragmentBinding: FollowFragmentBinding?
+    ) {
+        homeFragmentBinding?.apply {
+            errLayout.mainNotFound.visibility = View.GONE
+            progress.visibility = View.VISIBLE
+            recyclerHome.visibility = View.GONE
+        }
+    }
+
+    override fun onErrorState(
+        homeFragmentBinding: HomeFragmentBinding?,
+        followFragmentBinding: FollowFragmentBinding?,
+        message:String?
+    ) {
+        homeFragmentBinding?.apply {
+            errLayout.apply {
+                mainNotFound.visibility = View.VISIBLE
+                if (message == null){
+                    emptyText.text = resources.getString(R.string.not_found)
+                }else{
+                    emptyText.text = message
+                }
+            }
+            progress.visibility = View.GONE
+            recyclerHome.visibility = View.GONE
+        }
+    }
+
+    override fun onDestroyView() {
+        homeBinding.recyclerHome.adapter = null
+        _homeBinding = null
+        super.onDestroyView()
+    }
 }
