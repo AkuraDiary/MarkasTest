ea78c1ad1f8ea5e06880e63c552c177f366d97e2
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-22 11:46:20 +0700

    added onViewCreated

--- app/src/main/java/com/asthiseta/bismillahtest/follow/FollowFragment.kt
+++ app/src/main/java/com/asthiseta/bismillahtest/follow/FollowFragment.kt
@@ -1,11 +1,16 @@
 package com.asthiseta.bismillahtest.follow
 
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.asthiseta.bismillahtest.R
 import com.asthiseta.bismillahtest.databinding.FollowFragmentBinding
 import com.asthiseta.bismillahtest.databinding.HomeFragmentBinding
 import com.asthiseta.bismillahtest.util.ShowState
+import com.asthiseta.bismillahtest.util.TypeView
 import com.asthiseta.core.ui.UserAdapter
+import com.shashank.sony.fancytoastlib.FancyToast
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class FollowFragment : Fragment() , ShowState{
@@ -18,6 +23,39 @@ class FollowFragment : Fragment() , ShowState{
 
     private var type: String? = null
     private val followViewModel: FollowViewModel by viewModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            username = it.getString(USERNAME).toString()
+            type = it.getString(TYPE)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        followAdapter = UserAdapter(arrayListOf()) { user, _ ->
+            FancyToast.makeText(context, user, FancyToast.LENGTH_SHORT, FancyToast.INFO, false).show()
+        }
+
+        followBinding.recylerFollow.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            adapter = followAdapter
+        }
+
+        when(type) {
+            resources.getString(R.string.following) -> followViewModel.setFollow(username, TypeView.FOLLOWING)
+            resources.getString(R.string.followers) -> followViewModel.setFollow(username, TypeView.FOLLOWER)
+            else -> onErrorState(followFragmentBinding = followBinding, message = null)
+        }
+
+        observeFollow()
+    }
+
+    private fun observeFollow() {
+        TODO("Not yet implemented")
+    }
 
     override fun onSuccessState(
         homeFragmentBinding: HomeFragmentBinding?,
