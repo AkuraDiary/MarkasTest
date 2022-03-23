e11461b21e237cbe25c153529b330789991e14ba
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-23 14:48:20 +0700

    finally added MyApplication

--- app/src/main/java/com/asthiseta/bismillahtest/MyApplication.kt
+++ app/src/main/java/com/asthiseta/bismillahtest/MyApplication.kt
@@ -1,6 +1,29 @@
 package com.asthiseta.bismillahtest
 
 import android.app.Application
+import com.asthiseta.bismillahtest.di.useCaseModule
+import com.asthiseta.bismillahtest.di.viewModelModule
+import com.asthiseta.core.di.networkModule
+import com.asthiseta.core.di.repositoryModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin{
+            androidLogger(Level.NONE)
+            androidContext(this@MyApplication)
+            modules(
+                listOf(
+                    networkModule,
+                    repositoryModule,
+                    useCaseModule,
+                    viewModelModule
+                )
+            )
+        }
+    }
 }
