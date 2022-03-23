1813225459c35616cfdaf9020120a7f3358ef0e7
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-23 07:37:38 +0700

    added code in main activity

--- app/src/main/java/com/asthiseta/bismillahtest/ui/MainActivity.kt
+++ app/src/main/java/com/asthiseta/bismillahtest/ui/MainActivity.kt
@@ -2,11 +2,42 @@ package com.asthiseta.bismillahtest.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.asthiseta.bismillahtest.R
+import com.asthiseta.bismillahtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding : ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        setSupportActionBar(mainBinding.toolbar)
+
+        navController = findNavController(R.id.nav_host)
+        NavigationUI.setupWithNavController(mainBinding.bottomNavView, navController)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        navController.addOnDestinationChangedListener{_, destination, _ ->
+            when(destination.id){
+                R.id.detail_destination -> mainBinding.bottomNavView.visibility = View.GONE
+                else -> mainBinding.bottomNavView.visibility = View.VISIBLE
+            }
+        }
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        navController.navigateUp(appBarConfiguration)
+        return super.onSupportNavigateUp()
     }
 }
