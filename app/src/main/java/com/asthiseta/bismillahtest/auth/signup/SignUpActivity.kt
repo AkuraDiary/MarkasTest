f74acc75bf712cf7b11c61c2315aed707af5f752
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-29 06:20:13 +0700

    added base code for sign up activity

--- app/src/main/java/com/asthiseta/bismillahtest/auth/signup/SignUpActivity.kt
+++ app/src/main/java/com/asthiseta/bismillahtest/auth/signup/SignUpActivity.kt
@@ -4,13 +4,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.asthiseta.bismillahtest.R
 import com.asthiseta.bismillahtest.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var auth : FirebaseAuth
     private var binding : ActivitySignUpBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
+
+        val btnSignUp = binding!!.btnSSigned
+        val etEmail = binding!!.etSEmailAddress
+        val etConfPass = binding!!.etSConfPassword
+        val etPass = binding!!.etSPassword
+        val tvRedirectLogin = binding!!.tvRedirectLogin
+
+        auth = Firebase.auth
+
+        btnSignUp.setOnClickListener {
+            signUpUser()
+        }
+    }
+
+    private fun signUpUser() {
+        TODO("Not yet implemented")
     }
 
     override fun onDestroy() {
