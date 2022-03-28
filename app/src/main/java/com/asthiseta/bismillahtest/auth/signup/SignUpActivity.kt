67edef931c83d0888b42c522acaa1cf0631e550e
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-28 11:03:34 +0700

    changed to using binding

--- app/src/main/java/com/asthiseta/bismillahtest/auth/signup/SignUpActivity.kt
+++ app/src/main/java/com/asthiseta/bismillahtest/auth/signup/SignUpActivity.kt
@@ -3,10 +3,18 @@ package com.asthiseta.bismillahtest.auth.signup
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.asthiseta.bismillahtest.R
+import com.asthiseta.bismillahtest.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    private var binding : ActivitySignUpBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+    }
+
+    override fun onDestroy() {
+        binding = null
+        super.onDestroy()
     }
 }
