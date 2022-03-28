8a4004942ad121cc8ffa012124695de762c8cd3e
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-28 10:55:06 +0700

    added SignUp Activity

---
+++ app/src/main/java/com/asthiseta/bismillahtest/auth/signup/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.asthiseta.bismillahtest.auth.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.asthiseta.bismillahtest.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
