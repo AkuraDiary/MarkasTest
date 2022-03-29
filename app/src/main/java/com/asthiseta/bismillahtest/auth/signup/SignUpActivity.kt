643cdc25137a00ca822161acac760d83972a3a99
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-29 16:27:28 +0700

    added click listener into signin button

--- app/src/main/java/com/asthiseta/bismillahtest/auth/signup/SignUpActivity.kt
+++ app/src/main/java/com/asthiseta/bismillahtest/auth/signup/SignUpActivity.kt
@@ -3,6 +3,10 @@ package com.asthiseta.bismillahtest.auth.signup
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.asthiseta.bismillahtest.R
 import com.asthiseta.bismillahtest.auth.login.LoginActivity
 import com.asthiseta.bismillahtest.databinding.ActivitySignUpBinding
@@ -13,20 +17,29 @@ import com.google.firebase.ktx.Firebase
 class SignUpActivity : AppCompatActivity() {
     private lateinit var auth : FirebaseAuth
     private var binding : ActivitySignUpBinding? = null
+
+
+    private var btnSignUp : Button? = null
+    private var etEmail : EditText? = null
+    private var etConfPass = binding!!.etSConfPassword
+    private var etPass = binding!!.etSPassword
+    private var tvRedirectLogin = binding!!.tvRedirectLogin
+    private val loginIntent = Intent(this@SignUpActivity, LoginActivity::class.java)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
 
-        val btnSignUp = binding!!.btnSSigned
-        val etEmail = binding!!.etSEmailAddress
-        val etConfPass = binding!!.etSConfPassword
-        val etPass = binding!!.etSPassword
-        val tvRedirectLogin = binding!!.tvRedirectLogin
+        btnSignUp = binding!!.btnSSigned
+        etEmail = binding!!.etSEmailAddress
+        etConfPass = binding!!.etSConfPassword
+        etPass = binding!!.etSPassword
+        tvRedirectLogin = binding!!.tvRedirectLogin
 
         auth = Firebase.auth
 
-        btnSignUp.setOnClickListener {
+        btnSignUp!!.setOnClickListener {
             signUpUser()
         }
 
@@ -38,11 +51,47 @@ class SignUpActivity : AppCompatActivity() {
     }
 
     private fun signUpUser() {
-        TODO("Not yet implemented")
+        val email = etEmail?.text.toString()
+        val pass = etPass.text.toString()
+        val confirmPassword = etConfPass.text.toString()
+
+        // check pass
+        when{
+            email.isBlank()->{
+                error("Email can't be blank")
+            }
+            pass.isBlank()->{
+                error("Password can't be blank")
+            }
+            confirmPassword.isBlank()->{
+                error("This field can't be blank")
+            }
+        }
+
+        if (pass != confirmPassword) {
+            Toast.makeText(this, "Password and Confirm Password do not match", Toast.LENGTH_SHORT)
+                .show()
+            return
+        }
+        // If all credential are correct
+        // We call createUserWithEmailAndPassword
+        // using auth object and pass the
+        // email and pass in it.
+        auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(this) {
+            if (it.isSuccessful) {
+                Toast.makeText(this, "Successfully Singed Up, Please Login", Toast.LENGTH_SHORT).show()
+                // move into login activity
+                startActivity(loginIntent)
+                finish()
+            } else {
+                Toast.makeText(this, "Singed Up Failed!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun onDestroy() {
         binding = null
+
         super.onDestroy()
     }
 }
